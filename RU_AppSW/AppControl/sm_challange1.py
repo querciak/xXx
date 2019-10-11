7cc3a97102ea4ff645dc8d681c5fec6e7ff84aba
Tamas Faitli <tamasfaitli@gmail.com>
2019-10-11 23:15:20 +0300

    improving SM for challange 1

--- RU_AppSW/AppControl/sm_challange1.py
+++ RU_AppSW/AppControl/sm_challange1.py
@@ -1,9 +1,12 @@
 # state machine for challange one
 
 #imports:
-import Sensing
+from Sensing import Sensing
 from pybricks.parameters import Port, Color
+from Actions import Actions
 
+action = Actions()
+sensing = Sensing()
 
 ''' STATES for challange1
 0 'init'
@@ -20,29 +23,56 @@ STATES = ['init','reachstripe','turningleft','maze_followline',
 'mazeturn','mazepressbutton','mazeturnaround','mazewait']
 
 sm_challange1_init_timer_threshold = 1000 # 1sec
+mazeturn_counter_threshold = 1500 # 1,5 sec -> time to turn 90 degrees
+searching_line_turn_threshold = 100
 
-def isTimerPassed():
-    if current_time > sm_challange1_init_timer_threshold:
+
+def isTimerPassed(counter, threshold):
+    if  counter > threshold:
         return True
     else:
         return False
 
 def stripe_reached():
     #take value from color sensor
-    current_color = Sensing.Sensing.get_color()
+    current_color = sensing.get_color()
     if current_color == Color.WHITE:
         return True
     else:
         return False
 
+def yellow_button_reached():
+    #check whether color sensor shows yellow
+    current_color = sensing.get_color()
+    if current_color == Color.YELLOW:
+        return True
+    else:
+        return False
+
+def line_ended():
+    # check whether color sensor does not show white anymore
+    current_color = sensing.get_color()
+    if current_color != Color.WHITE:
+        return True
+    else:
+        return False    
+
+def wall_reached():
+    #TODO determine whether wall is reached using ultrasonic sensor
+    pass
+
 def sm_challange1_main():
     global CURRENT_STATE
     global transition_to
+    global sm_challange1_init_timer
+    global mazeturn_timer
+    global searching_line_turn_timer
 
     if CURRENT_STATE == STATES[0]: # init
         #check valid transition
         #timer()
-        if isTimerPassed() == True:
+        if isTimerPassed(sm_challange1_init_timer,sm_challange1_init_timer_threshold) == True:
+            sm_challange1_init_timer = 0
             transition_to = 1
 
     elif CURRENT_STATE == STATES[1]: # reachstripe
@@ -52,7 +82,7 @@ def sm_challange1_main():
             transition_to = 2 # turningleft
 
     elif CURRENT_STATE == STATES[2]: # turningleft
-        if turning_timer_passed() == True:
+        if isTimerPassed(searching_line_turn_timer,searching_line_turn_threshold) == True:
             transition_to = 1
 
     elif CURRENT_STATE == STATES[3]: # maze_followline
@@ -64,7 +94,7 @@ def sm_challange1_main():
 
     elif CURRENT_STATE == STATES[4]: # mazeturn
         # check turning timer
-        if maze_turningtimer_passed() == True:
+        if isTimerPassed(mazeturn_timer,mazeturn_counter_threshold) == True:
             transition_to = 3
 
     elif CURRENT_STATE == STATES[5]: # mazepressbutton
@@ -82,11 +112,12 @@ def sm_challange1_main():
     # action
     if CURRENT_STATE == STATES[0]: # init
         # wait
-        pass
+        sm_challange1_init_timer += 1
 
     elif CURRENT_STATE == STATES[1]: # reachstripe
         # set speed value
-        pass
+        action.straight_speed = 100
+        action.steering_speed = 0
 
     elif CURRENT_STATE == STATES[2]: # turningleft
         pass
@@ -108,5 +139,15 @@ def sm_challange1_main():
 
 
 def sm_challange1_init():
+    #initiate variables used in sm_challange1.py
+    global CURRENT_STATE
+    global transition_to
+    global sm_challange1_init_timer
+    global mazeturn_timer
+    global searching_line_turn_timer
+
     CURRENT_STATE = STATES[0]
     transition_to = 0
+    sm_challange1_init_timer = 0
+    mazeturn_timer = 0
+    searching_line_turn_timer = 0
