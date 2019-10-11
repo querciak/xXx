fbcd37fc172cb8bad50736c94244676d18c3ac2a
Luca Scotton <lucascott1996@gmail.com>
2019-10-11 22:33:54 +0300

    Actions

---
+++ RU_AppSW/Actions.py
@@ -0,0 +1,17 @@
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+
+class Actions:
+
+    def __init__(self):
+        super().__init__()
+        self.steering_speed = 45
+        self._left_motor = Motor(Port.A)
+        self._right_motor = Motor(Port.B)
+        self.wheels = DriveBase(self._left_motor, self._right_motor, wheel_diameter=56, axle_track=114)
+
+
+    def turn(self, angle=0):
+        self.wheels.drive_time(0, 45, 1000)
