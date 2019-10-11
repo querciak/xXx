6f08867889215d9361f8e75147291f9fba12733b
Luca Scotton <lucascott1996@gmail.com>
2019-10-11 23:44:35 +0300

    Sensing fix

--- RU_AppSW/Sensing.py
+++ RU_AppSW/Sensing.py
@@ -14,6 +14,18 @@ class Sensing():
         self.color_sensor = ColorSensor(Port.S3)
         self.__col_queue = deque(maxlen=self.color_samples)
 
+    @staticmethod
+    def most_frequent(l):
+        counter = 0
+        num = [0]
+
+        for i in l:
+            curr_frequency = l.count(i)
+            if (curr_frequency > counter):
+                counter = curr_frequency
+                num = i
+        return num
+
     def get_color(self):
         i = 0
         self.__col_queue.append(self.color_sensor.color())
